4663f1291e7b3cdf6ea19af6779c9d338c6a6492
bo7owers <7orres.rene@proton.me>
2022-12-02 21:55:50 -0500

    Delay showing reset btn

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ const hasSetup = ref(false)
 const setup = ref(null)
 const delivery = ref(null)
 const showDelivery = ref(false)
+const showBtn = ref(false)
 
 async function getJoke() {
   try {
@@ -12,17 +13,25 @@ async function getJoke() {
     setup.value = apiResp.data.setup
     delivery.value = apiResp.data.delivery
     hasSetup.value = true
-    console.log(apiResp.data)
-    return apiResp
   } catch (error) {
     console.log(error)
     hasSetup.value = false
   }
 }
+const delayBtn = () => setInterval(() => (showBtn.value = true), 700)
 
-const resetJoke = computed(() => {
-  return window.location.reload()
-})
+const drumRoll = () => {
+  showDelivery.value = true
+  delayBtn()
+}
+
+const reset = () => {
+  hasSetup.value = false
+  setup.value = null
+  delivery.value = null
+  showDelivery.value = null
+  showBtn.value = false
+}
 </script>
 <template>
   <div class="w-full h-full flex flex-col justify-center items-center">
@@ -37,7 +46,7 @@ const resetJoke = computed(() => {
       <div class="setup-delivery flex flex-col" v-if="hasSetup">
         <p v-if="!showDelivery">{{ setup }}</p>
         <button
-          @click="showDelivery = true"
+          @click="drumRoll"
           class="bg-rose-600 hover:bg-rose-800 text-white font-bold py-2 px-4 rounded mx-auto mt-3"
           v-if="!showDelivery"
         >
@@ -46,8 +55,8 @@ const resetJoke = computed(() => {
         <p v-if="showDelivery">{{ delivery }}</p>
         <button
           class="bg-rose-800 hover:bg-rose-900 text-white font-bold py-2 px-4 rounded mx-auto mt-3"
-          v-if="showDelivery"
-          @click="resetJoke"
+          v-if="showBtn && showDelivery"
+          @click="reset"
         >
           Reset
         </button>
